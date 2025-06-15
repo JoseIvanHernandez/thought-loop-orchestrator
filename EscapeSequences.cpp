8b9380e30bf2b8a65a1a8a53baa94f0735cc1be5
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-06-15 06:35:12 -0700

    Update EscapeSequences.cpp
    Same situation here.

---
+++ EscapeSequences.cpp
@@ -0,0 +1,8 @@
+// IC01_EscapeSequences
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Line 1\nLine 2\tTabbed\nLine 3 with a backslash: \\\nQuoted: \"Hello, world!\"\n"
+		return 0;
+	}
