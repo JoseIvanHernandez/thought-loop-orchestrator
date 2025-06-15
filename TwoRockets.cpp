a3377be722a2e5a6e96369b5dabfa331c3e17015
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-06-15 06:38:11 -0700

    Update TwoRockets.cpp
    was not sure if you wanted United States inside the rocket or our names or anything in particular.

---
+++ TwoRockets.cpp
@@ -0,0 +1,16 @@
+// IC01_TwoRockets
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "   /\\      /\\\n"
+		      "  /  \\    /  \\\n"
+		      " /    \\  /    \\\n"
+		      "+------+ +------+\n"
+		      "|  JO  | |  JO  |\n"
+		      "|SE IV | |SE IV |\n"
+		      "+------+ +------+\n"
+		      "   ||       ||\n"
+		      "   ||       ||\n";
+	return 0;
+}
