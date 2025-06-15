68ce1626b68623c9b9a8cf57fd0cf5526a9ff26b
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-06-15 06:33:52 -0700

    Update Assignment01.cpp
    I tried using Visual Studio Code, but when I committed, nothing reflected on the file, so I just copied and pasted here.

---
+++ Assignment01.cpp
@@ -0,0 +1,9 @@
+// IC01_PersonallInformation with my name and major
+// each piece of information on a seperate line
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Jose Ivan Hernandez\nSystems Administration and Artificial Intelligence\n";
+	return 0;
+}
