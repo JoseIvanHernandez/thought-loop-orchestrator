a2e0f67603389f2227417e3a0ef26f6e65f08caf
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-06-23 07:22:53 +0000

    Fix BMR calculation syntax and finalize chocolate bar output

--- IC04_BasalMetabolicRate.cpp
+++ IC04_BasalMetabolicRate.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Declare our toys
-    double weight, height, age;
-    double brmWoman, bmrMan;
-    double chocoBarsWoman, chocoBarsMan;
-
+    double weight, height;
+    int age;
+    
     // Ask user to give us the pieces
     cout << "Enter your weight in pounds: ";
     cin >> weight;
@@ -16,16 +16,23 @@ int main() {
     cin >> age;
 
     // Use the Harris-Benedict formulas
-    bmrWoman = 655 + (4.35 * weight) * (4.7 * height) - (4.7 * age);
-    bmrMan = 66 (6.23 * weight) + (12.7 * height) - (6.8 * age);
+    double bmrWoman = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+    double bmrMan = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
 
     // Chocolate bar math
-    chocoBarsWoman = brWoman / 230.0;
-    chocoBarsMan = bmrMan / 230.0;
+    double barsForWoman = bmrWoman / 230.0;
+    double barsForMan = bmrMan / 230.0;
 
     // Output the results
-    cout << "Calories needed for a woman: " << bmrWoman << endl;
-    cout << "That's about " << chocoBarsMan << "chocolate bars.\n";
+    cout << fixed << setprecision(2);
+    cout << "Calories needed per day\n";
+    cout << "Woman: " << bmrWoman << " calories\n";
+    cout << "Man:  " << bmrMan << "calories\n";
+    
+    cout << "Chocolate bars per day:\n";
+    cout << "Woman: " << barsForWoman << " bars\n";
+    cout << "Man  : " << barsForMan << " bars\n";
     
     return 0;
+
 }
