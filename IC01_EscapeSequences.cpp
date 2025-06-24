8da1b79025f9ef703b646f8f958c9f28e6ab441a
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-06-24 18:42:50 +0000

    Restored and submitted IC01_EscapeSequences assignment

---
+++ IC01_EscapeSequences.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Escape sequences in C++:\n"
+         << "Newline: \\n\n"
+         << "Tab: \\t\n"
+         << "Backslash: \\\\\n"
+         << "Single quote: \\\'\n"
+         << "Double quote: \\\"\n"
+         << "Question mark: \\?\n"
+         << "Alert (beep): \\a\n"
+         << "Backspace: \\b\n"
+         << "Form feed: \\f\n"
+         << "Carriage return: \\r\n"
+         << "Vertical tab: \\v\n";
+    return 0;
+}
