ce645b49da25dd1f4a79341c1b13997b529cd56e
Jose Ivan <128977855+JoseIvanHernandez@users.noreply.github.com>
2025-07-07 01:47:55 +0000

    IC10_IncaQuipu assignment completed

---
+++ IC10_IncaQuipu.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// fraw knots using unctions
+
+void drawKnots(int count) {
+    if (count == 0) {
+        cout << "--- (no knots)\n"
+        return;
+    }
+    for (int 1 = 0; i < count; i++) {
+        cout << "@ ";
+    }
+    cout << "\n";
+}
+int main() {
+    int number;
+
+    cout << "============================\n";
+    cout << "      Inca Quipu Encode\n";
+    cout << "============================\n";
+
+    cout << "Enter a number (0-999): ";
+    cin >> number;
+
+    if (number < 0 || number > 999) {
+        cout << "Error! Number out of range. Please enter between 0 and 9999.\n";
+        return 1;
+    }
+
+    int hundreds = number / 100;
+    int tens = (number / 10) % 10;
+    int ones = number % 10;
+
+    cout << "n\Visual Quipu for " << number << ":\n";
+    cout << "Hundreds (" << hundreds << "): ";
+    drawKnots(hundreds);
+
+    cout << "Tens (" << tens << "):     ";
+    drawKnots(tens);
+
+    cout << "Ones (" << ones << ")     ";
+    drawKnots(ones);
+
+    return 0;
+}
